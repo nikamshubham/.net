8d0772d4327a2bf865521a88665cb87a27740421
SHUBHAM NIKAM <65148970+nikamshubham@users.noreply.github.com>
2020-12-05 00:07:32 +0530

    Day 5 Assignment

---
+++ Day 5/Q1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayAssignQ1
+{
+    class Program
+    {
+        private static int flag = 0;
+        static void Main(string[] args)
+        {
+                      
+            Employee[] arr = new Employee[3];
+          
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write("Enter Emp No = ");
+                int eno = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter Emp Name = ");
+                string name = Console.ReadLine();
+                Console.Write("Enter Salary = ");
+                decimal sal = Convert.ToDecimal(Console.ReadLine());
+                arr[i] = new Employee(eno,name,sal);
+            }
+            
+            foreach (var item in arr)
+            {
+                item.Details();
+            }
+
+            Console.Write("Enter Emp No to be searched  - ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            decimal dd = arr[0].salary;
+           
+            for (int i = 0; i < arr.Length; i++)
+			{
+                if(dd<arr[i].salary)
+                {
+                    dd = arr[i].salary; 
+                }
+                if(n==arr[i].empno){
+                    Console.WriteLine("Employee Found");
+                    Console.WriteLine("Name - "+arr[i].empname); 
+                    flag = 1;
+                }
+                
+        	}
+            if(flag!=1){
+                Console.WriteLine("Employee Not Found");
+            }
+            Console.WriteLine("====================================");
+            Console.WriteLine("Max salary between all Employee = "+dd);
+            
+        }
+    }
+    public class Employee
+    {
+        public int empno 
+        { 
+            set; 
+            get; 
+        }
+        public string empname 
+        {
+            set; 
+            get; 
+        }
+        public decimal salary 
+        {
+            set; 
+            get;
+        }
+
+        public Employee(int no=0,string name="NoName",decimal sal=0)
+        {
+            empno = no;
+            empname = name;
+            salary = sal;
+            //Console.WriteLine("const");
+        }
+
+        public void Details()
+        {
+            Console.WriteLine("Employee No = "+empno+" ,Name = " +empname+",Salary = "+salary);
+        }
+    }
+}
+/*
+  Create an array of Employee class objects
+  Accept details for all Employees
+  Display the Employee with highest Salary
+  Accept EmpNo to be searched. Display all details for that employee.
+ */
---
+++ Day 5/Q2.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructStudent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] obj = new Student[5];
+    
+            for (int i = 0; i < obj.Length; i++)
+            {
+                Console.Write("Enter Name = ");
+                string name = Console.ReadLine();
+                Console.Write("Enter RollNo = ");
+                int rollno = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter Marks = ");
+                int marks = Convert.ToInt32(Console.ReadLine());
+                obj[i].PName = name;
+                obj[i].PRollNo = rollno;
+                obj[i].PMarks = marks;
+            }
+            //Console.WriteLine(obj.Length);
+            //Console.WriteLine(obj[0]);
+            //Console.WriteLine(obj[0].PName);
+            //Console.WriteLine(obj[1]);
+            //Console.WriteLine(obj[1].PName);
+
+            foreach (var item in obj)
+            {
+                item.Display();
+            }
+            
+        }
+    }
+
+    public struct Student
+    {
+        private string name;
+        private int rollno;
+        private decimal marks;
+
+        public string PName
+        {
+            get { return name; }
+            set
+            {
+                if (value != null)
+                    name = value;
+                else Console.WriteLine("Name should not be empty");
+            }
+        }
+
+        public int PRollNo
+        {
+            get { return rollno; }
+            set
+            {
+                if (value > 0 || value < 139)
+                    rollno = value;
+                else Console.WriteLine("Incorrect Rollno");
+            }
+        }
+
+        public decimal PMarks
+        {
+            get { return marks; }
+            set
+            {
+                if (value > 32)
+                {
+                    //Console.WriteLine("Passed");
+                    marks = value;
+                }
+                    
+
+            }
+        }
+
+        public Student(string name="NoName",int rollno=0,decimal marks=0)
+        {
+            this.name = "";
+            this.rollno = 0;
+            this.marks = 0;
+            PName = name;
+            PRollNo = rollno;
+            PMarks = marks;
+        }
+      
+        public void Display()
+        {
+            Console.WriteLine("Name = "+ name + " ,Roll no = "+rollno+" ,Marks = "+marks);
+        }
+
+    }
+}
+
+/*
+Create a struct Student with the following properties. Put in appropriate validations.
+string Name
+int RollNo
+decimal Marks
+
+Create a parameterized constructor.
+
+Create an array to accept details for 5 students
+ */
