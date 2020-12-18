05c98671211abfc22bd3c378f512c1603924b4cd
SHUBHAM NIKAM <65148970+nikamshubham@users.noreply.github.com>
2020-12-18 20:11:23 +0530

    Basic Web Based Application

---
+++ WebBasedApplication/Controllers/DefaultController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstApplication.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //public string Index()
+        //{
+        //    return "Hello <b>World</b>";
+        //}
+        public ActionResult Method2()
+        {
+            return View();
+        }
+    }
+}
